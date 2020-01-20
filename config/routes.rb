00f3e42ee4af4df5ba7cc6870a1110adad6ac5f8
mikefab <mikefabrikant@gmail.com>
2020-01-20 19:03:08 +0000

    Set main route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :equipment
+  root 'equipment#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
