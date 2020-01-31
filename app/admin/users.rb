e2b751c56c06b28f386c48e56afe06e24e942dac
mikefab <mikefabrikant@gmail.com>
2020-01-31 11:22:01 +0000

    simplify user form in admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,30 @@
 ActiveAdmin.register User do
+  permit_params :email, :password, :password_confirmation
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin, :org_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin, :org_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :org
+      f.input :admin
+      # f.input :password
+      # f.input :password_confirmation
+    end
+    f.actions
+  end
 
 end
